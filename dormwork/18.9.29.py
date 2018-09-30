f1b4faa7c3d5229f953f17442aa61b992f31c1f2
zhuzipei <978285124@qq.com>
2018-09-30 15:22:36 +0800

    boolean

--- dormwork/18.9.29.py
+++ dormwork/18.9.29.py
@@ -11,13 +11,15 @@ def check(string):
 		if char in left + right:
 			if char in left:
 				cache.append(char)
+				print(cache)
 			elif char in right and char == right[left.index(cache[-1])]:
 				cache.pop(-1)
+				print(cache)
 			else:
 				return False
 
 def main():
-	if not check(input()):
+	if check(input()) is False:
 		print("no")
 	else:
 		print("yes")
